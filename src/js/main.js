490e9548177295960cb26691d005713870a44fa3
caseydev <becool_kumar_leon@yahoo.com>
2015-10-17 18:40:05 +0545

    Test Project Completed

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.image-holder').last().addClass('last-child');
+  $('.image-holder').colorbox({
+    width: 400,
+    height: 400,
+    rel: 'group1',
+    loop: false,
+    onComplete: function () {
+      var timeAnim = setTimeout($.colorbox.next, 500);
+      if($.colorbox.element().hasClass('last-child')) {
+        clearTimeout(timeAnim);
+        $.colorbox.close();
+      }
+    }
+  }).first().click();
+
+});
